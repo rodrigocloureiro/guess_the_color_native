92ef4d5eb092316200a8880d110a0845e23eba85
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-06-28 20:05:05 -0300

    estrutura inicial

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight : 70;
+// console.log(statusBarHeight);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light' />
+      <View style={styles.titleContainer}>
+        <Text style={[styles.title, {color: '#ffaa00'}]}>Guess</Text>
+        <Text style={[styles.title, {color: '#ccff00'}]}> the </Text>
+        <Text style={[styles.title, {color: '#00ccff'}]}>Color</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#242424',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: statusBarHeight,
+    paddingHorizontal: 20,
+  },
+  titleContainer: {
+    flexDirection: 'row',
+  },
+  title: {
+    color: '#fafafa',
+    fontSize: 40,
+    fontWeight: '700',
+  },
+  text: {
+    color: '#fafafa',
+    fontSize: 18,
   },
 });
